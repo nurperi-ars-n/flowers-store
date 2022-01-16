e087e72754bda572e2d28f9bc57cefd9a68c779c
Nurperi Nogoibaeva <nurperiarss@gmail.com>
2022-01-16 12:55:14 +0600

    added pagination-component

--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -1,29 +1,23 @@
-import React from 'react'
-import classes from '../../assets/styles/Content.module.css'
-
-import Card from '../UI/Card'
-import { useSelector } from 'react-redux'
-
-
-
-
-
-
-
+import React from "react";
+import classes from "../../assets/styles/Content.module.css";
+import Card from "../UI/Card";
+import { useSelector } from "react-redux";
+import StyledPagination from "../UI/Pagination";
 
 export default function Content() {
-
-
-    const flowers = useSelector(state => state.flowers.flowers)
-
-
-    return (
-        <div className={classes.container}>
-
-            <div className={classes.flowersBlock}>
-                {flowers.map(item => <Card key={item.id} className={classes.flower} item={item} />)}
-            </div>
-        
-        </div>
-    )
+	const flowers = useSelector((state) => state.flowers.flowers);
+	return (
+		<div className={classes.container}>
+			<div className={classes.flowersBlock}>
+				{flowers.map((item) => (
+					<Card
+						key={item.id}
+						className={classes.flower}
+						item={item}
+					/>
+				))}
+				<StyledPagination count={"5"} />
+			</div>
+		</div>
+	);
 }
--- src/utills/constants/general.js
+++ src/utills/constants/general.js
@@ -3,15 +3,6 @@ export const ROLES = {
 };
 
 export const ROUTES = {
-<<<<<<< HEAD
-	FLOWERS: "/flowers",
-	ADMIN: "/admin",
-	LOG_xIN: "/login",
-	ABOUT_US: "/about",
-	CART: "/cart",
-	ACCESSDENIED: "/accessdenied",
-	SIGN_UP: "/sign_up",
-=======
   FLOWERS: "/flowers",
   ADMIN: "/admin",
   LOGIN: "/login",
@@ -20,5 +11,4 @@ export const ROUTES = {
   ACCESSDENIED: "/accessdenied",
   ADMIN_FLOWERS: "flowers",
   ADMIN_USERS: "users",
->>>>>>> 3bac0c973e38a792dad87f5e610953b9ae776dfc
 };
